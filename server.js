61c67e68b1da410bfa32f47a4237b31d0436529d
Erick Morales Silva <erick.silva@localhost.localdomain>
2018-05-02 17:56:00 -0500

    v0.0.4 - Fixing some issues.

--- server.js
+++ server.js
@@ -1,48 +1,13 @@
-// var WebSocketServer = require('websocket').server;
-// var http = require('http');
-
-// var server = http.createServer(function(request, response) {
-//   // process HTTP request. Since we're writing just WebSockets
-//   // server we don't have to implement anything.
-// });
-// server.listen(1337, function() { });
-
-// // create the server
-// wsServer = new WebSocketServer({
-//   httpServer: server
-// });
-
-// // WebSocket server
-// wsServer.on('request', function(request) {
-//   var connection = request.accept(null, request.origin);
-
-//   connection.sendUTF('echo from wsServer: Connection accepted...');
-
-//   // This is the most important callback for us, we'll handle
-//   // all messages from users here.
-//   connection.on('message', function(message) {
-//     if (message.type === 'utf8') {
-//       // process WebSocket message
-//       connection.sendUTF('echo from wsServer: ' + JSON.stringify(message));
-//     }
-//   });
-
-//   connection.on('close', function(connection) {
-//     // close user connection
-//   });
-// });
-
 var WebSocketServer = require('websocket').server;
-// var http = require('http');
+var http = require('http');
 var uuidv4 = require('uuid/v4');
 var express = require('express');
 
-var server = express().use((req, res) => {
-    console.log((new Date()) + ' Received request for ' + req.url);
-    req.writeHead(404);
-    req.end();
+var app = express();
 
-}).listen(8080, () => {
+var server = http.createServer(app);
+
+server.listen(8080, () => {
     console.log((new Date()) + ' Server is listening on port 8080...');
 });
 
